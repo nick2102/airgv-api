e64dff7514f73bed4698085f3b30cb84edc21b58
Nikola Nikoloski <n.nikoloski21@gmail.com>
2020-06-29 02:40:39 +0200

    as

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const connectionString = 'mongodb://ntp_super:M0therlode.321@localhost:27017/admin?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false';
+const connectionString = 'mongodb://ntp_super:M0therlode.321@localhost:27017/airgv-aqi?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false';
 
 mongoose.connect(connectionString, {
     useNewUrlParser: true,
